df57196b1dc5cb98a83a2249d50abcdfd0cb3d8d
pevullo-jpg <pevullo@gmail.com>
2026-03-23 12:32:34 +0100

    Add files via upload

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -7,6 +7,7 @@ class AppCollections {
   static const String appSettings = 'app_settings';
   static const String drivePdfImports = 'drive_pdf_imports';
   static const String doctorPatientLinks = 'doctor_patient_links';
+  static const String families = 'families';
   static const String prescriptionIntakes = 'prescription_intakes';
   static const String parserReferenceValues = 'parser_reference_values';
 }
